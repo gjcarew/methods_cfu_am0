5de425d96302ef817bd2b39f8bf4cae370b2afda
Gavin Carew <gjcarew@gmail.com>
2022-06-09 13:29:48 -0700

    Add answers to define_and_id.rb

--- define_and_id.rb
+++ define_and_id.rb
@@ -12,16 +12,28 @@
 
 "Hello World".include?("Hello")
 
+# The .include? method evaluates whether the parameter string is included in the string it is called on.
+# The argument "Hello" is passed, and it is included in the string .include? was called on.
+# The return value is true
 
 "Hello World".end_with?("Hello")
 
+# The .end_with? method evaluates whether the parameter string is at the end of the string it is called on.
+# The argument "Hello" is passed, but it is not the last five characters of the string.
+# The return value is false
 
 "Hello World".end_with?("rld")
 
+# In this case, the argument "rld" is passed, which are the last three characters of the input sting .end_with? was called on.
+# The return value is false
 
 12.even?
 
+# The .even? method evaluates whether the integer it is called on is even.
+# The return is boolean.
+# In this case, The return value is true
 
 18.next
-
-
+#
+# The .next method is a tool to add 1 to the integer it is called on.
+# There are no arguments. The return value is 19.
